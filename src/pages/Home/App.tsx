491724f86507b07e0c7a32ec4ff2bbac116d44a8
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-03 15:11:18 -0300

    style(Home): add style to title Cards section

--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -7,14 +7,17 @@ export const App = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <div className={styles.cardsContainer}>
-        <Card name="Introdução e Preparatório" classes="3/3 aulas" />
-        <Card name="Conceito de Sistemas" classes="4/4 aulas" />
-        <Card name="Lógica de programação" classes="16/16 aulas" />
-        <Card name="Conceito de Sistemas" classes="4/4 aulas" />
-        <Card name="Projeto Mobile" classes="10/10 aulas" />
-        <Card name="Conceito de Sistemas" classes="4/4 aulas" />
-      </div>
+      <section className={styles.cardsSection}>
+        <h1>Módulos</h1>
+        <div className={styles.cardsContainer}>
+          <Card name="Introdução e Preparatório" classes="3/3 aulas" />
+          <Card name="Conceito de Sistemas" classes="4/4 aulas" />
+          <Card name="Lógica de programação" classes="16/16 aulas" />
+          <Card name="Conceito de Sistemas" classes="4/4 aulas" />
+          <Card name="Projeto Mobile" classes="10/10 aulas" />
+          <Card name="Conceito de Sistemas" classes="4/4 aulas" />
+        </div>
+      </section>
     </div>
   );
 };
