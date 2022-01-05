789e42e98c78a20a5baa2c92cb307ce3c00f9a69
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-05 18:18:17 -0300

    feat: react-router-dom, Login and Register emptys

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { App } from './pages/Home/App';
 
 import './global.scss';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
