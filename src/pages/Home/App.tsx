06fd04fce8546278c4b454770edafb0ca1e51803
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-05 23:34:34 -0300

    refactor: show classes in modules container

--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import Modal from 'react-modal';
+import { BiArrowBack } from 'react-icons/bi';
 
 import { ModuleCard } from '../../components/ModuleCard';
 import { ModuleForm } from '../../components/ModuleForm';
 import { Header } from '../../components/Header';
+
 import api from '../../services/api';
 
 import styles from './styles.module.scss';
+
 import { ClassesForm } from '../../components/ClassesForm';
 import { ClassesCard } from '../../components/ClassesCard';
 
@@ -45,13 +48,16 @@ export const App = () => {
   const [classes, setClasses] = useState<ClassesProps[]>([]);
   const [modulesForm, setModulesForm] = useState(false);
   const [classesActives, setClassesActives] = useState<ClassesProps[]>([]);
-  // const [moduleActive, setModuleActive] = useState();
+  const [moduleActive, setModuleActive] = useState('Módulos');
+  const [showModules, setShowModules] = useState(true);
+  const [showClasses, setShowClasses] = useState(false);
 
   useEffect(() => {
     api.getModules().then(response => setModulos(response.data));
     api.getClasses().then(response => setClasses(response.data));
   }, []);
 
+  // Função para calcular quantas aulas existem em cada módulo
   const classesQuantity = (id: string) => {
     return classes.filter(c => c.module_id === id).length;
   };
@@ -60,49 +66,78 @@ export const App = () => {
     setIsOpen(false);
   };
 
-  const handleClassModal = () => {
+  const handleOpenClassModal = () => {
     setModulesForm(false);
     setIsOpen(true);
   };
 
-  const handleModuleModal = () => {
+  const handleOpenModuleModal = () => {
     setModulesForm(true);
     setIsOpen(true);
   };
 
   const handleModuleActive = (event: React.MouseEvent<HTMLButtonElement>) => {
     const target = event.target as Element;
-    const teste = classes.filter(c => c.module_id === target.id);
-    setClassesActives(teste);
+    // Função que, quando clica em um Módulo, pega o id dele, compara com o array de aulas e retorna apenas as aulas que fazem parte daquele módulo
+    const grades = classes.filter(c => c.module_id === target.id);
+    setClassesActives(grades);
+
+    const findModulo = modulos.find(m => m.id === target.id);
+
+    setModuleActive(findModulo?.name as string);
+    setShowModules(false);
+    setShowClasses(true);
+  };
+
+  const handleMoveBack = () => {
+    setModuleActive('Módulos');
+    setShowModules(true);
+    setShowClasses(false);
   };
 
   return (
     <>
       <div className={styles.container}>
         <Header
-          openModuleModal={handleModuleModal}
-          openClassModal={handleClassModal}
+          openModuleModal={handleOpenModuleModal}
+          openClassModal={handleOpenClassModal}
         />
+
         <section className={styles.cardsSection}>
-          <h1>Módulos</h1>
-          <div className={styles.cardsContainer}>
-            {modulos.length !== 0 ? (
-              modulos.map(modulo => (
-                <ModuleCard
-                  handleModuleActive={handleModuleActive}
-                  key={modulo.id}
-                  id={modulo.id}
-                  name={modulo.name}
-                  classes={classesQuantity(modulo.id)}
-                />
-              ))
-            ) : (
-              <span>Não existem módulos criados. :(</span>
+          <h1>
+            {moduleActive !== 'Módulos' && (
+              <button type="button" onClick={() => handleMoveBack()}>
+                <BiArrowBack />
+              </button>
             )}
+            {moduleActive !== 'Módulos' ? moduleActive : 'Módulos'}
+          </h1>
+          <div className={styles.cardsContainer}>
+            {showModules && modulos.length !== 0
+              ? modulos.map(modulo => (
+                  <ModuleCard
+                    handleModuleActive={handleModuleActive}
+                    key={modulo.id}
+                    id={modulo.id}
+                    name={modulo.name}
+                    classes={classesQuantity(modulo.id)}
+                  />
+                ))
+              : !showClasses && <span>Não existem módulos criados. :(</span>}
+
+            {showClasses && classesActives.length !== 0
+              ? classesActives.map(c => (
+                  <ClassesCard
+                    key={c.id}
+                    name={c.name}
+                    classDate={c.class_date}
+                  />
+                ))
+              : !showModules && <span>Nenhuma aula encontrada :(</span>}
           </div>
         </section>
 
-        <section className={styles.classesSection}>
+        {/* <section className={styles.classesSection}>
           <h1>Aulas</h1>
           <div className={styles.classesContainer}>
             {classesActives.length !== 0
@@ -115,7 +150,7 @@ export const App = () => {
                 ))
               : 'Nenhuma aula encontrada :('}
           </div>
-        </section>
+        </section> */}
       </div>
       <Modal
         isOpen={modalIsOpen}
