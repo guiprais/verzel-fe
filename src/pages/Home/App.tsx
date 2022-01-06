75791d8174d778513e6ee11eea9413a7182a78c5
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-06 16:06:07 -0300

    feat: add useClassesApi

--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { BiArrowBack } from 'react-icons/bi';
 import { AiFillEdit } from 'react-icons/ai';
 
@@ -7,35 +7,24 @@ import { ModuleForm } from '../../components/ModuleForm';
 import { Modal } from '../../components/Modal';
 import { Header } from '../../components/Header';
 
-import api from '../../services/api';
-
 import styles from './styles.module.scss';
 
 import { ClassesForm } from '../../components/ClassesForm';
 import { ClassesCard } from '../../components/ClassesCard';
 import { useModulesApi } from '../../hooks/useModulesApi';
-
-type ClassesProps = {
-  class_date: string;
-  id: string;
-  module_id: string;
-  name: string;
-};
+import { useClassesApi } from '../../hooks/useClassesApi';
 
 export const App = () => {
   const [modalIsOpen, setIsOpen] = useState(false);
-  const [classes, setClasses] = useState<ClassesProps[]>([]);
   const [modulesForm, setModulesForm] = useState(false);
-  const [classesActives, setClassesActives] = useState<ClassesProps[]>([]);
   const [moduleActive, setModuleActive] = useState('Módulos');
   const [showModules, setShowModules] = useState(true);
   const [showClasses, setShowClasses] = useState(false);
   const [showEditForm, setShowEditForm] = useState(false);
-  const { modules, getModule } = useModulesApi();
 
-  useEffect(() => {
-    api.getClasses().then(response => setClasses(response.data));
-  }, []);
+  const { modules, getModule } = useModulesApi();
+  const { classes, setClasses, classesActives, setClassesActives } =
+    useClassesApi();
 
   // Função para calcular quantas aulas existem em cada módulo
   const classesQuantity = (id: string) => {
@@ -69,16 +58,40 @@ export const App = () => {
     setShowClasses(true);
   };
 
+  const showModulesContent = () => {
+    return modules.length !== 0
+      ? modules.map(modulo => (
+          <ModuleCard
+            handleModuleActive={handleModuleActive}
+            key={modulo.id}
+            id={modulo.id}
+            name={modulo.name}
+            classes={classesQuantity(modulo.id)}
+          />
+        ))
+      : !showClasses && <span>Não existem módulos criados. :(</span>;
+  };
+
+  const showClassesContent = () => {
+    return classesActives.length !== 0
+      ? classesActives.map(c => (
+          <ClassesCard key={c.id} name={c.name} classDate={c.class_date} />
+        ))
+      : !showModules && <span>Nenhuma aula encontrada :(</span>;
+  };
+
   const handleMoveBack = () => {
     setModuleActive('Módulos');
     setShowModules(true);
     setShowClasses(false);
+    setShowEditForm(false);
   };
 
   const handleEditModule = () => {
     const moduleObject = getModule(moduleActive);
 
-    setShowEditForm(true);
+    setShowEditForm(!showEditForm);
+    setShowClasses(!showClasses);
   };
 
   return (
@@ -104,27 +117,9 @@ export const App = () => {
             )}
           </h1>
           <div className={styles.cardsContainer}>
-            {showModules && modules.length !== 0
-              ? modules.map(modulo => (
-                  <ModuleCard
-                    handleModuleActive={handleModuleActive}
-                    key={modulo.id}
-                    id={modulo.id}
-                    name={modulo.name}
-                    classes={classesQuantity(modulo.id)}
-                  />
-                ))
-              : !showClasses && <span>Não existem módulos criados. :(</span>}
-
-            {showClasses && classesActives.length !== 0
-              ? classesActives.map(c => (
-                  <ClassesCard
-                    key={c.id}
-                    name={c.name}
-                    classDate={c.class_date}
-                  />
-                ))
-              : !showModules && <span>Nenhuma aula encontrada :(</span>}
+            {showModules && showModulesContent()}
+            {showClasses && showClassesContent()}
+            {showEditForm && <ModuleForm />}
           </div>
         </section>
       </div>
